a4595d8d85e3124c1a3bab4cff5b165c2e6109bc
NicholasKunguKamau <nicholaskungukamau@gmail.com>
2022-06-27 01:25:22 +0300

    function to listen our even when we select dog breed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ start()
 //  Function for Creating Dropdown of Dog breeds
 function createBreedList(breedList) {
 document.getElementById("breed").innerHTML = `
-<select>
+<select onchange="loadByBreed(this.value)">
         <option>Choose Yor Favorite Dog Breed</option>
        ${Object.keys(breedList).map(function(breed) {
 return `<option>${breed}</option>`
@@ -17,4 +17,13 @@ return `<option>${breed}</option>`
 
     </select>
 ` 
+}
+
+//Function to listen our event
+async function loadByBreed(breed) {
+if (breed != "Choose Yor Favorite Dog Breed") {
+    const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`)
+const data = await response.json()
+console.log(data)
+   } 
 }
