725fcb6c73d3ea3b45606e193206b8148b136090
NicholasKunguKamau <nicholaskungukamau@gmail.com>
2022-06-27 03:40:26 +0300

    adding catch incase of failure in fetch of data

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ let deleteFirstPhotoDelay
 
 //Fetch data 
 async function start() {
-    const response = await fetch("https://dog.ceo/api/breeds/list/all")
+    //adding catch incase of error occuring when fetching data
+try {
+        const response = await fetch("https://dog.ceo/api/breeds/list/all")
 const data = await response.json()
 createBreedList(data.message)
+} catch (e) {
+    console.log("Dog Breed List Not Fetched.")
+}
 }
 start()
 
