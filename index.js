759c432189022542bb4ae9c33e541bef1239af32
NicholasKunguKamau <nicholaskungukamau@gmail.com>
2022-06-27 03:13:15 +0300

    cancel timer when different dog breed is choosen

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+//Global Scope Variable to
+let timer
+let deleteFirstPhotoDelay
+
 //Fetch data 
 async function start() {
     const response = await fetch("https://dog.ceo/api/breeds/list/all")
@@ -31,17 +35,19 @@ createSlideShow(data.message)
 //Function to Create Slides
 function createSlideShow(images) {
     let currentPosition = 0
+    clearInterval(timer)
+    clearTimeout(deleteFirstPhotoDelay) 
 document.getElementById("slideshow").innerHTML = `
  <div class="slide" style="background-image: url('${images[0]}')"> </div>
   <div class="slide" style="background-image: url('${images[1]}')"> </div>
         `
         currentPosition +=2
         //Time taken  to show next slide 
-        setInterval(nextSlide, 3000)
+       timer = setInterval(nextSlide, 3000)
         function nextSlide() {
             document.getElementById("slideshow").insertAdjacentHTML("beforeend", ` <div class="slide" style="background-image: url('${images[currentPosition]}')"> </div>`)
 //Time to wait before next slide is shown 
-            setTimeout(function(){
+           deleteFirstPhotoDelay = setTimeout(function(){
                 //reomve shown slide
                 document.querySelector(".slide").remove()
             }, 1000)
